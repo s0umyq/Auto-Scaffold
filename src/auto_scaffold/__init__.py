335c68e873bdaed49c8e1c818f26d64118174f1f
mishradeeptibala <mishradeeptibala@gmail.com>
2026-08-05 15:30:35 +0530

    Fix

--- src/auto_scaffold/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Auto-Scaffold CLI package."""
-
-from auto_scaffold.cli import cli
-
-__all__ = ["cli"]
--- src/auto_scaffold/gui/__init__.py
+++
@@ -1,5 +0,0 @@
-"""GUI package."""
-
-from auto_scaffold.gui.server import app, run_gui
-
-__all__ = ["app", "run_gui"]
